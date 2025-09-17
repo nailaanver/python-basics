071e424323acfee93e659d5044c497f445c95037
Fathima Naila <fathimanaila71@gmail.com>
2025-09-17 21:00:54 +0530

    twelth commit

---
+++ __pycache__/simple_contact_manager.py
@@ -0,0 +1,11 @@
+# def store_contacts():
+#     contacts = []
+    
+#     def add_contact():
+#         name = input("Enter the contact name").strip().title()
+#         if name in contacts:
+#             print(f"Error: A contact with the name '{name}' already exists.")
+#         return
+#     phone = input("Enter a phone ")
+#     contacts[name] = phone
+    # print(f"Contact '{name}' added successfully.")
